da069942dc7120b6b3b131bc0836a5a252ca0aaf
Ramai Saud <ramai.saud@impressico.local>
2025-12-26 14:04:57 +0530

    fix: rename entry point to streamlit_app.py to satisfy Snowflake requirements

--- snowflake_app/setup.sql
+++ snowflake_app/setup.sql
@@ -5,6 +5,6 @@ GRANT USAGE ON SCHEMA app_schema TO APPLICATION ROLE app_public;
 
 CREATE OR REPLACE STREAMLIT app_schema.data_governance_streamlit
   FROM '/streamlit'
-  MAIN_FILE = 'app.py';
+  MAIN_FILE = 'streamlit_app.py';
 
 GRANT USAGE ON STREAMLIT app_schema.data_governance_streamlit TO APPLICATION ROLE app_public;
